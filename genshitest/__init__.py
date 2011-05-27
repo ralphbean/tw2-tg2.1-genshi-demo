405abe314436f7416fbcd93303a0bc7dc781170f
Ralph Bean <ralph.bean@gmail.com>
2011-05-27 18:27:19 -0400

    Initial commit.

---
+++ genshitest/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""The genshitest package"""
---
+++ genshitest/lib/helpers.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+"""WebHelpers used in genshitest."""
+
+from webhelpers import date, feedgenerator, html, number, misc, text
